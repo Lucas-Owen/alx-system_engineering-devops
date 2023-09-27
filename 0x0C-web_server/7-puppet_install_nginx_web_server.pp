5e7e9656a7e38e6018d2800729e0590e8c1f0c2a
Lucas Sangura <sanguraowens@gmail.com>
2023-09-27 19:01:00 +0300

    Add puppet configurations

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,25 @@
 # This manifest sets up a configuration for an ngix server on a linux machine
+package {'nginx':
+  ensure => installed,
+  notify => Exec['setup redirect', 'setup 404']
+}
+
+exec {'setup 404':
+  command     => "/usr/bin/sed -i \'51 a \\\terror_page 404 \\/404.html;\\n\' /etc/nginx/sites-available/default",
+  refreshonly => true
+}
+
+exec {'setup redirect':
+  command     => "/usr/bin/sed -i \'47 a \\\tlocation \\/redirect_me \\{\\n\t\treturn 301 https:\\/\\/www.youtube.com\\/watch?v=QH2-TGUlwu4;\\n\t\\}\\n\' /etc/nginx/sites-available/default",
+  refreshonly => true
+}
+
+file {'/var/www/html/index.html':
+  ensure  => file,
+  content => "Hello World!\n",
+}
+
+file {'/var/www/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page\n",
+}
