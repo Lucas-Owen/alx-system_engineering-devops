8d1fb5a1ac8c5f4c35552e32ed08537ee24f62cf
Lucas Sangura <sanguraowens@gmail.com>
2023-11-08 15:52:34 +0300

    Fix manifest

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # This puppet manifest renames a file
 
-Exec {'rename':
-  command => '/usr/bin/mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp'
+exec { 'rename' :
+  command => '/bin/mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp';
 }
